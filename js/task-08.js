88a839f7f1d250327d0a41a83e2d464eac4aa6d0
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-01-24 19:01:00 +0100

    Add task-8 solution

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const loginForm = document.querySelector("form.login-form");
 
+loginForm.addEventListener("submit", handleLoginSubmit);
+
+/**
+ * * Handles form validation and prints form values into console as an object.
+ * @param {Event} event - the event that has occurred.
+ * Prevent default page refresh behaviour upon form submit.
+ * In case any of inputs is empty - informs user using alert, prefent empty form submit.
+ * Prints submitted form values in form of an object.
+ * Clean all inputs upon formsubmit.
+ */
+function handleLoginSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert("All fields must be completed");
+    return;
+  }
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(data);
+
+  event.currentTarget.reset();
+}
